8e1de19ca7687aeab5b7697b00793fdefa9c0e52
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-25 14:06:29 +0300

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,20 @@
 #include "Animal.h"
 
 //define mammal class here
+class Mammal : public Animal {
+private:
+    string furColor;
 
+public:
+    Mammal();
+    Mammal(string n, int a, bool h, string color);
+    ~Mammal();
+
+    void setFurColor(string color);
+    string getFurColor() const;
+
+    void display() const override;
+};
 
 
 
