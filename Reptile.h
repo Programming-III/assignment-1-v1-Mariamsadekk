8cf5ff9041610b4830191ce60d7625fe84911a7e
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-25 14:07:37 +0300

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -4,7 +4,20 @@
 #include "Animal.h"
 
 //define reptile class here
+class Reptile : public Animal {
+private:
+    bool isVenomous;
 
+public:
+    Reptile();
+    Reptile(string n, int a, bool h, bool venom);
+    ~Reptile();
+
+    void setIsVenomous(bool venom);
+    bool getIsVenomous() const;
+
+    void display() const override;
+};
 
 
 
