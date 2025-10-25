34661be191dc9035977488cab3b59c7a8b8d1d4d
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-25 14:06:51 +0300

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,7 +5,20 @@
 
 //define class bird here 
 
+class Bird : public Animal {
+private:
+    float wingSpan;
 
+public:
+    Bird();
+    Bird(string n, int a, bool h, float span);
+    ~Bird();
+
+    void setWingSpan(float span);
+    float getWingSpan() const;
+
+    void display() const override;
+};
 
 
 
