902b5a79918eac9869e9e45e01fd577f2a8fda8c
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-25 14:00:57 +0300

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,22 +1,80 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
-class Animal{
-  private:
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
 
+public:
+    Animal();
+    Animal(string n, int a, bool h);
+    virtual ~Animal();
 
+    void setName(string n);
+    string getName() const;
 
-  public:
+    void setAge(int a);
+    int getAge() const;
 
+    void setIsHungry(bool h);
+    bool getIsHungry() const;
 
-
-
-
+    virtual void display() const;
+    virtual void feed();
 };
 
+// Mammal subclass
+class Mammal : public Animal {
+private:
+    string furColor;
+
+public:
+    Mammal();
+    Mammal(string n, int a, bool h, string color);
+    ~Mammal();
+
+    void setFurColor(string color);
+    string getFurColor() const;
+
+    void display() const override;
+};
+
+// Bird subclass
+class Bird : public Animal {
+private:
+    float wingSpan;
+
+public:
+    Bird();
+    Bird(string n, int a, bool h, float span);
+    ~Bird();
+
+    void setWingSpan(float span);
+    float getWingSpan() const;
+
+    void display() const override;
+};
+
+// Reptile subclass
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+
+public:
+    Reptile();
+    Reptile(string n, int a, bool h, bool venom);
+    ~Reptile();
+
+    void setIsVenomous(bool venom);
+    bool getIsVenomous() const;
+
+    void display() const override;
+};
 
 #endif
